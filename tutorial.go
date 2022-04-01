0c6177f3157150ec47231899d682e04ae99bd4ff
Tatiana Johnson <Tatianarjo@gmail.com>
2022-03-31 22:02:51 -0500

    creates input for quiz game

--- tutorial.go
+++ tutorial.go
@@ -3,5 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello world!")
+	fmt.Println("Welcome to my Crazy Quiz Game!")
+
+	fmt.Printf("Enter your name: ")
+	var name string
+
+	fmt.Scan(&name)
+
+	fmt.Printf("Hello, %v, welcome to the game!", name)
 }
+
+//33:18
